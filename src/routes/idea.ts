358dea89efa425aa64f77b38b57aff9d14cde14e
trungshin6969 <trungnpvgcs18890@fpt.edu.vn>
2022-02-16 17:06:59 +0700

    Complete CRUD for Idea

--- src/routes/idea.ts
+++ src/routes/idea.ts
@@ -1,20 +1,27 @@
 import { Router } from "express";
 import { getRepository, Repository } from "typeorm";
-import { body, param } from "express-validator";
+import { checkSchema, param } from "express-validator";
 import { StatusCodes, ReasonPhrases } from "http-status-codes";
-import { Idea, Permissions } from "@app/database";
+import { AcademicYear, Idea, Permissions, User, Category } from "@app/database";
 import { asyncRoute, permission } from "@app/utils";
 import _ from "lodash";
 
 export function ideaRouter(): Router {
 	const router = Router();
-	const repository: Repository<Idea> = getRepository(Idea);
+	const repositoryIdea: Repository<Idea> = getRepository(Idea);
+	const repositoryUser: Repository<User> = getRepository(User);
+	const repositoryYear: Repository<AcademicYear> = getRepository(AcademicYear);
+	const repositoryCategory: Repository<Category> = getRepository(Category);
 
 	router.get(
 		"/",
 		permission(Permissions.IDEA_GET_ALL),
 		asyncRoute(async (req, res) => {
-			res.json(await repository.find());
+			res.json(
+				await repositoryIdea.find({
+					relations: ["user", "comment", "document", "reactions", "views", "academicYear"]
+				})
+			);
 		})
 	);
 
@@ -24,7 +31,11 @@ export function ideaRouter(): Router {
 		param("id").isInt(),
 		asyncRoute(async (req, res) => {
 			if (req.validate()) {
-				res.json(await repository.findOneOrFail(req.params.id));
+				res.json(
+					await repositoryIdea.findOneOrFail(req.params.id, {
+						relations: ["user", "comment", "document", "reactions", "views", "academicYear"]
+					})
+				);
 			}
 		})
 	);
@@ -32,10 +43,49 @@ export function ideaRouter(): Router {
 	router.post(
 		"/",
 		permission(Permissions.IDEA_CREATE),
-		body("content").exists().isString(),
+		checkSchema({
+			user: {
+				in: "body",
+				exists: true,
+				custom: {
+					options: (value: any) => {
+						return !_.isInteger(value) ? Promise.reject() : repositoryUser.findOneOrFail({ id: value });
+					}
+				}
+			},
+			academicYear: {
+				in: "body",
+				exists: true,
+				custom: {
+					options: (value: any) => {
+						return !_.isInteger(value) ? Promise.reject() : repositoryYear.findOneOrFail({ id: value });
+					}
+				}
+			},
+			categories: {
+				in: "body",
+				exists: true,
+				custom: {
+					options: (value: any) => {
+						return !_.isInteger(value) ? Promise.reject() : repositoryCategory.findOneOrFail({ id: value });
+					}
+				}
+			},
+			content: {
+				in: "body",
+				exists: true,
+				isString: true
+			}
+		}),
 		asyncRoute(async (req, res) => {
 			if (req.validate()) {
-				res.json(await repository.save(repository.create({ content: req.body.content })));
+				const idea = repositoryIdea.create({
+					content: req.body.content,
+					user: req.body.user,
+					academicYear: req.body.academicYear,
+					categories: req.body.categories
+				});
+				res.json(await repositoryIdea.save(idea));
 			}
 		})
 	);
@@ -43,14 +93,54 @@ export function ideaRouter(): Router {
 	router.put(
 		"/:id",
 		permission(Permissions.IDEA_UPDATE),
-		param("id").isInt(),
+		checkSchema({
+			id: {
+				in: "params",
+				isInt: true
+			},
+			user: {
+				in: "body",
+				exists: true,
+				custom: {
+					options: (value: any) => {
+						return !_.isInteger(value) ? Promise.reject() : repositoryUser.findOneOrFail({ id: value });
+					}
+				}
+			},
+			academicYear: {
+				in: "body",
+				exists: true,
+				custom: {
+					options: (value: any) => {
+						return !_.isInteger(value) ? Promise.reject() : repositoryYear.findOneOrFail({ id: value });
+					}
+				}
+			},
+			categories: {
+				in: "body",
+				exists: true,
+				custom: {
+					options: (value: any) => {
+						return !_.isInteger(value) ? Promise.reject() : repositoryCategory.findOneOrFail({ id: value });
+					}
+				}
+			},
+			content: {
+				in: "body",
+				exists: true,
+				isString: true
+			}
+		}),
 		asyncRoute(async (req, res) => {
 			if (req.validate()) {
-				const idea = await repository.findOneOrFail(req.params.id);
-				
-				idea.content = _.get(req.body, "content", idea.content);
+				const idea = await repositoryIdea.findOneOrFail(req.params.id);
 
-				res.json(await repository.save(idea));			
+				idea.content = _.get(req.body, "content", idea.content);
+				idea.user = _.get(req.body, "user", idea.user);
+				idea.academicYear = _.get(req.body, "academicYear", idea.academicYear);
+				idea.categories = _.get(req.body, "categories", idea.categories);
+
+				res.json(await repositoryIdea.save(idea));
 			}
 		})
 	);
@@ -60,7 +150,7 @@ export function ideaRouter(): Router {
 		permission(Permissions.IDEA_DELETE),
 		param("id").isInt(),
 		asyncRoute(async (req, res) => {
-			await repository.delete(req.params.id);
+			await repositoryIdea.delete(req.params.id);
 			res.status(StatusCodes.OK).send(ReasonPhrases.OK);
 		})
 	);
